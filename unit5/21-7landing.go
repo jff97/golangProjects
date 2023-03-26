81395e37e69dbeac43d2e84f1886113635c238bf
John Fox <107318784+jff97@users.noreply.github.com>
2023-03-26 11:58:54 -0500

    Update 21-7landing.go

--- unit5/21-7landing.go
+++ unit5/21-7landing.go
@@ -2,21 +2,26 @@ package main
 import (
 	"fmt"
 	"encoding/json"
+	"os"
 )
 
 func main() {
 	type location struct {
-		name string 'json:"name"'
-		lat  float64 'json:latitude'
-		long float64 'json:longitude'
+		Name string `json:"name"`
+		Lat  float64 `json:latitude`
+		Long float64 `json:longitude`
   	}
   
   	locations := []location{
-		{name: "Bradbury Landing", lat: -4.5895, long: 137.4417},
-		{name: "Columbia Memorial Station", lat: -14.5684, long: 175.472636},
-		{name: "Challenger Memorial Station", lat: -1.9462, long: 354.4734},
+		{Name: "Bradbury Landing", Lat: -4.5895, Long: 137.4417},
+		{Name: "Columbia Memorial Station", Lat: -14.5684, Long: 175.472636},
+		{Name: "Challenger Memorial Station", Lat: -1.9462, Long: 354.4734},
   	}
 
-	bytes := json.Marshal(locations)
+	bytes, err := json.Marshal(locations)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+  }
 	fmt.Println(string(bytes))
 }
